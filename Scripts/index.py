686ec863fa46697d9603b974ab10b6a55a40dcfe
Neyroph <116799727+Neyroph@users.noreply.github.com>
2023-12-21 09:08:52 +0100

    center button

--- Scripts/index.py
+++ Scripts/index.py
@@ -14,14 +14,25 @@ if __name__ == "__main__":
     buttons = pygame.sprite.Group()
 
     def window():
-        b1 = Button("PLAY", pos=(100, 100),
-                    fontsize=36,
-                    width=200,
-                    height=75,
-                    colors="white on grey",
-                    hover_colors="white on red",
-                    command=lambda: print("clicked right now"),
-                    buttons=buttons)
+
+        screen_width, screen_height = pygame.display.get_surface().get_size()
+        center_x = screen_width // 2
+        center_y = screen_height // 2
+
+        button_width = 400
+        button_height = 150
+        button_x = center_x - button_width // 2
+        button_y = center_y - button_height // 2
+
+        b1 = Button("PLAY", pos=(button_x, button_y),
+                fontsize=50,
+                width=button_width,
+                height=button_height,
+                colors="white on darkgrey",
+                hover_colors="white on grey",
+                command=lambda: print("clicked right now"),
+                buttons=buttons)
+
 
     window()
     is_running = True
